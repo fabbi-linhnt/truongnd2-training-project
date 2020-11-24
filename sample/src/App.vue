508733c66901511265e3e85e189abadd57a788f8
truongnd-fabbi <truongnd2@fabbi.io>
2020-11-24 12:46:03 +0700

    integrate bootstrapvue

--- sample/src/App.vue
+++ sample/src/App.vue
@@ -4,11 +4,34 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+
+    <div class="container">
+      <b-row>
+        <b-col md="auto">
+          <b-calendar v-model="value" @context="onContext" locale="en-US"></b-calendar>
+        </b-col>
+      </b-row>
+    </div>
     <router-view />
   </div>
 </template>
 
+<script>
+  import Vue from 'vue';
+  import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+
+  Vue.use(BootstrapVue);
+  Vue.use(IconsPlugin);
+
+  export default {
+    name: 'App',
+  }
+
+</script>
+
 <style lang="scss">
+@import 'node_modules/bootstrap/scss/bootstrap.scss';
+@import 'node_modules/bootstrap-vue/src/index.scss';
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
